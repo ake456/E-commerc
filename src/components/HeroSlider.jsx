e14ad1b0208a70cf81efeba62981f847306d43e5
ake456 <ansb70610@gmail.com>
2026-08-03 16:43:05 +0300

    fix2

--- src/components/HeroSlider.jsx
+++ src/components/HeroSlider.jsx
@@ -4,6 +4,9 @@ import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
 import 'swiper/css/pagination';
 import { Link } from 'react-router-dom';
+import banner1 from "../../img/banner_Hero1.jpg";
+import banner2 from "../../img/banner_Hero2.jpg";
+import banner3 from "../../img/banner_Hero3.jpg";
 
 import { Autoplay, Pagination, Navigation } from 'swiper/modules';
 
@@ -24,7 +27,7 @@ function HeroSlider() {
                                 <p>Windows Xp/10/7/8 Ps3, Tv Box</p>
                                 <Link to="/" className='btn'>Shop Now</Link>
                             </div>
-                            <img src="/src/img/banner_Hero1.jpg" alt="hero" />
+                            <img src="banner1" alt="hero" />
                         </SwiperSlide>
                         <SwiperSlide>
                             <div className="content">
@@ -33,7 +36,7 @@ function HeroSlider() {
                                 <p>upport 3.5 mm jack audio input</p>
                                 <Link to="/" className='btn'>Shop Now</Link>
                             </div>
-                            <img src="/src/img/banner_Hero2.jpg" alt="hero" />
+                            <img src="banner2" alt="hero" />
                         </SwiperSlide>
                         <SwiperSlide>
                             <div className="content">
@@ -42,7 +45,7 @@ function HeroSlider() {
                                 <p>AAC HD Sound Quality</p>
                                 <Link to="/" className='btn'>Shop Now</Link>
                             </div>
-                            <img src="/src/img/banner_Hero3.jpg" alt="hero" />
+                            <img src="banner3" alt="hero" />
                         </SwiperSlide>
                     </Swiper>
                 </div>
